c9668f1fbd6c2cedd23cc9b6ff33917f4a00555f
rudic <zhenya_mironov_92@list.ru>
2021-03-19 16:52:26 +0400

    norm addaptive

--- js/economy.js
+++ js/economy.js
@@ -1,14 +1,2 @@
-function init(){
-let par=document.getElementsByClassName('proect');
-//par.innerHTML="ggwp";
-console.log(par.innerHTML);
-//par.setAttribute("class", "ggwp");
-let parid=document.getElementById('proect');
-parid.setAttribute("id","redteext");
-let fet = document.getElementById('icon');
-//fet.innerHTML="fet";
-let alt = fet.getAttribute("class");
- console.log(alt);
-}
-window.onload=init;
+
 document.querySelector('.range').value = 0;
