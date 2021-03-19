5dffc9e00e319a63ab26c5978f2354cbe73f0d70
rudic <zhenya_mironov_92@list.ru>
2021-03-19 13:52:20 +0400

    adaptiv

--- js/economy.js
+++ js/economy.js
@@ -1,3 +1,2 @@
-alert (32);
 document.querySelector('.range').value = 0;
-document.querySelector('.px-3').querySelector('.h1').querySelector('p').hidden = true;
+//document.querySelector('.px-3').querySelector('.h1').querySelector('p').hidden = true;
